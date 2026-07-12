1902950c8475d2244f5fe45aad7d151da5470316
imcz-dotcom <chuanzhe0904@gmail.com>
2026-07-12 17:33:53 +0800

    Add xAxis configuration to ModelPopularityChart for better label readability

--- frontend/components/dashboard/ModelPopularityChart.tsx
+++ frontend/components/dashboard/ModelPopularityChart.tsx
@@ -9,7 +9,16 @@ interface ModelPopularityChartProps {
 export default function ModelPopularityChart({ data }: ModelPopularityChartProps) {
   return (
     <ChartCard title="Model Popularity" description="Most common SSD models in the current inventory.">
-      <BarChart h={300} data={data} dataKey="name" series={[{ name: 'count', label: 'SSDs', color: 'grape.6' }]} tickLine="y" withLegend legendProps={{ verticalAlign: 'top', align: 'right' }} />
+      <BarChart
+        h={300}
+        data={data}
+        dataKey="name"
+        series={[{ name: 'count', label: 'SSDs', color: 'grape.6' }]}
+        tickLine="y"
+        withLegend
+        legendProps={{ verticalAlign: 'top', align: 'right' }}
+        xAxisProps={{ angle: -45, textAnchor: 'end', interval: 0, height: 80, tick: { fontSize: 10 } }}
+      />
     </ChartCard>
   );
 }
