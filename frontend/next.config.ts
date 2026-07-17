d17da36031b559406601893d2211903481c0fc47
Imcz-dotcom <chuanzhe0904@gmail.com>
2026-07-18 00:41:32 +0800

    fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  turbopack: {},
   webpack: (config, { dev }) => {
     if (dev) {
       config.watchOptions = {
